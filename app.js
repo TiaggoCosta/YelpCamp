459eb823c85dd94e6b4bed9b97230cf359e13364
Tiago Costa <tiago.tagcosta@gmail.com>
2019-01-27 23:47:59 +0000

    add bootstrap4

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ app.use(express.static(__dirname + "/public"));
 app.use(methodOverride("_method"));
 app.use(flash());
 // seedDB(); //seed the database
-
+app.locals.moment = require('moment');
 // PASSPORT CONFIGURATION
 app.use(require("express-session")({
     secret: "Once again Rusty wins cutest dog!",
